e74ac7fc4640b1e7c37a29724172c9329cc4397e
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-11-03 15:25:36 -0800

    Added create new caanvas feature

---
+++ javascript.js
@@ -0,0 +1,50 @@
+const containerSize = 700;
+let gridSize = 16;
+
+function generateCanvas () {
+    const container = document.querySelector("#container");
+    let numberOfBoxes = gridSize * gridSize; 
+    let boxSize = `${containerSize / gridSize}px`;
+
+    const body = document.querySelector("body");
+    body.removeChild(container);
+
+    const newContainer = document.createElement("div");
+    newContainer.id = "container";
+    body.appendChild(newContainer);
+
+    for (let i = 0; i < numberOfBoxes; i ++){
+        const div = document.createElement("div");
+        div.classList.toggle("grid");
+        div.style.cssText = `width: ${boxSize}; height: ${boxSize}`;
+        newContainer.appendChild(div)
+    }    
+
+    let gridElements = document.querySelectorAll(".grid");
+
+    gridElements.forEach((grid) => {
+        grid.addEventListener("mouseover", (event) => {
+            grid.style.backgroundColor = "blue";
+            event.stopPropagation();
+        })
+    });
+
+}
+
+
+const newCanvas = document.querySelector("#newCanvas");
+
+newCanvas.addEventListener("click", (event) => {
+    userInput = prompt("Enter a number to define grid size for your canvas.");
+    userInput = Number(userInput);
+    console.log(userInput);
+    if (userInput <= 100 && Number.isInteger(userInput)) {
+        gridSize = userInput;
+    } else {
+        alert("Invalid grid size. Please select an integer from 1 to 100.");
+    }
+    event.stopPropagation();
+    generateCanvas();
+});
+
+generateCanvas();
